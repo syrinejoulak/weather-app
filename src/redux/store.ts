021a9186a351af4d2e83aa5f93deddd8fb80d0e5
syrinejoulak <syrinejoulak@gmail.com>
2022-10-31 22:23:05 +0100

    setup store for testing

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {
+  combineReducers,
+  configureStore,
+  PreloadedState,
+} from '@reduxjs/toolkit';
 
 import weatherSlice from './slices/weather';
 import modalSlice from './slices/modal';
@@ -10,7 +14,20 @@ export const store = configureStore({
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+// Create the root reducer separately so we can extract the RootState type
+
+const rootReducer = combineReducers({
+  weather: weatherSlice,
+  modal: modalSlice,
+});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
+export type AppStore = ReturnType<typeof setupStore>;
